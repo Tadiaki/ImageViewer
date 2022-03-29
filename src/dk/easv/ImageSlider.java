0bd6641958cef83bf2e048c1ab3f8a9ac78306f8
Tadiaki <n0m1z@hotmail.com>
2022-03-29 10:50:13 +0200

    refactored

--- src/dk/easv/ImageSlider.java
+++
@@ -1,4 +0,0 @@
-package dk.easv;
-
-public class ImageSlider {
-}
---
+++ src/dk/easv/Slideshow.java
@@ -0,0 +1,12 @@
+package dk.easv;
+
+import javafx.concurrent.Task;
+
+public class Slideshow extends Task {
+
+
+    @Override
+    protected Object call() throws Exception {
+        return null;
+    }
+}
